b235ce26f989c288f5c2418b609c89638b412da3
Anwar Ramadha <anwar.ramadha@gmail.com>
2018-01-27 20:48:27 +0700

    update to 256 char

--- extended_vigenere.py
+++ extended_vigenere.py
@@ -1,11 +1,5 @@
 import string 
 
-print("1. Encrypt Text")
-print("2. Encrypt File")
-print("3. Decrypt Text")
-print("4. Decrypt File")
-select = raw_input("Selection: ")
-
 def read_file():
 	filename = raw_input("File name: ")
 	file = open(filename, 'r')
@@ -22,8 +16,22 @@ def write_file(text):
 		file.close()
 		print("Saved!")
 
+extended_ascii = [chr(i) for i in xrange(256)]
+
+print("1. Encrypt Text")
+print("2. Encrypt File")
+print("3. Decrypt Text")
+print("4. Decrypt File")
+select = raw_input("Selection: ")
+
+inp = ''
+if select == "1" or select == "3":
+	print("\n1. Input from keyboard")
+	print("2. Input from text file")
+	inp = raw_input("Selection: ")
+
 if select == "1" or select == "2":
-	if select == "1":
+	if inp == "1":
 		plaintext = raw_input("Input plaintext: ")
 	else :
 		plaintext = read_file()
@@ -36,55 +44,46 @@ if select == "1" or select == "2":
 
 	for c in plaintext:
 
-		if c in string.printable:
+		if c in extended_ascii:
 			if key_char_itt == key_length:
 				key_char_itt = 0
 
-			plain_index = string.printable.index(c)
-			key_index = string.printable.index(key[key_char_itt])
+			plain_index = extended_ascii.index(c)
+			key_index = extended_ascii.index(key[key_char_itt])
 
 			key_char_itt += 1
 
-			chip_index = (plain_index + key_index) % len(string.printable)
+			chip_index = (plain_index + key_index) % len(extended_ascii)
 
-			cipher_text += string.printable[chip_index]
+			cipher_text += extended_ascii[chip_index]
 		else:
 			cipher_text += c
 
 	if select == "1":
-
-		print("\n1. Print look alike plaintext")
-		print("2. Print without whitespace")
-		print("3. Print in groups of 5 letters")
-
-		p = raw_input("Selection: ")
 		print("\nPlain Text: "+plaintext)
 
-		if p == "1":
-			print("cipher text: "+cipher_text)
-			write_file(cipher_text)
-		elif p == "2":
-			print("cipher text: "+cipher_text.replace(" ", ""))
-			write_file(cipher_text.replace(" ",""))
-		elif p == "3":
-			c_temp = cipher_text.replace(" ","")
-			text = ''
-			groups = 0
-			for c in c_temp:
-				if groups == 4:
-					text += ' '+c
-					groups = 0
-				else:
-					text += c
-					groups += 1
-			print("cipher text: "+text)
-			write_file(text)
+		print("Like plaintext: "+cipher_text)
+
+		c_temp = cipher_text.replace(" ","")
+		print("Without whitespace: "+c_temp)
+
+		text = ''
+		groups = 0
+		for c in c_temp:
+			if groups == 4:
+				text += ' '+c
+				groups = 0
+			else:
+				text += c
+				groups += 1
+		print("In 5 letters: "+text)
+		write_file(cipher_text)
 
 	else :
 		write_file(cipher_text)
 
 elif select == "3" or select == "4":
-	if select == "3":
+	if inp == "1":
 		ciphertext = raw_input("cipher Text: ")
 	else:
 		ciphertext = read_file()
@@ -96,18 +95,18 @@ elif select == "3" or select == "4":
 	plaintext = ''
 
 	for c in ciphertext:
-		if c in string.printable:
+		if c in extended_ascii:
 			if key_char_itt == key_length:
 				key_char_itt = 0
 
-			chip_index = string.printable.index(c)
-			key_index = string.printable.index(key[key_char_itt])
+			chip_index = extended_ascii.index(c)
+			key_index = extended_ascii.index(key[key_char_itt])
 
 			key_char_itt += 1
 
-			plain_index = (chip_index - key_index) % len(string.printable)
+			plain_index = (chip_index - key_index) % len(extended_ascii)
 			
-			plaintext += string.printable[plain_index]
+			plaintext += extended_ascii[plain_index]
 		else:
 			plaintext += c
 
